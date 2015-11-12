dde43175a7a655605e48d42192f76dafbb28ac5b
Edward Yun <edward.d.yun@vanderbilt.edu>
2015-11-12 11:37:36 -0600

    initial commit

---
+++ tinderForFood/FoodViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  tinderForFood
+//
+//  Created by Edward Yun on 11/12/15.
+//  Copyright © 2015 Edward Yun. All rights reserved.
+//
+
+import UIKit
+
+class FoodViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myLabel.text = "hello world"
+        
+        var label = UILabel(frame: CGRectMake(0, 0, 200, 21))
+        label.center = CGPointMake(160, 284)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "I'm a test label"
+        self.view.addSubview(label)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
