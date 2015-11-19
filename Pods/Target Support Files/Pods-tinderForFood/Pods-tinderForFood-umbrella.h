1e3cae17cadd598cb64164fc135d0a81fba5466e
Edward Yun <edward.d.yun@vanderbilt.edu>
2015-11-19 12:22:47 -0600

    basic swipping works

---
+++ Pods/Target Support Files/Pods-tinderForFood/Pods-tinderForFood-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tinderForFoodVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tinderForFoodVersionString[];
+
--- tinderForFood/FoodViewController.swift
+++ tinderForFood/FoodViewController.swift
@@ -7,27 +7,87 @@
 //
 
 import UIKit
+import Koloda
+import pop
+
+private let numberOfCards: UInt = 5
+private let frameAnimationSpringBounciness:CGFloat = 9
+private let frameAnimationSpringSpeed:CGFloat = 16
+private let kolodaCountOfVisibleCards = 2
+private let kolodaAlphaValueSemiTransparent:CGFloat = 0.1
 
 class FoodViewController: UIViewController {
 
-    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var kolodaView: UIView!
     
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        myLabel.text = "hello world"
-        
-        var label = UILabel(frame: CGRectMake(0, 0, 200, 21))
-        label.center = CGPointMake(160, 284)
-        label.textAlignment = NSTextAlignment.Center
-        label.text = "I'm a test label"
-        self.view.addSubview(label)
-        
+        kolodaView.alphaValueSemiTransparent = kolodaAlphaValueSemiTransparent
+        kolodaView.countOfVisibleCards = kolodaCountOfVisibleCards
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        self.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //MARK: IBActions
+    @IBAction func leftButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Left)
+    }
+    
+    @IBAction func rightButtonTapped() {
+        kolodaView?.swipe(SwipeResultDirection.Right)
+    }
+    
+    @IBAction func undoButtonTapped() {
+        kolodaView?.revertAction()
+    }
+    
+    //MARK: KolodaViewDataSource
+    func kolodaNumberOfCards(koloda: KolodaView) -> UInt {
+        return numberOfCards
+    }
+    
+    func kolodaViewForCardAtIndex(koloda: KolodaView, index: UInt) -> UIView {
+        return UIImageView(image: UIImage(named: "cards_\(index + 1)"))
+    }
+    func kolodaViewForCardOverlayAtIndex(koloda: KolodaView, index: UInt) -> OverlayView? {
+        return NSBundle.mainBundle().loadNibNamed("CustomOverlayView",
+            owner: self, options: nil)[0] as? OverlayView
+    }
+    
+    //MARK: KolodaViewDelegate
+    
+    func kolodaDidSwipedCardAtIndex(koloda: KolodaView, index: UInt, direction: SwipeResultDirection) {
+    }
+    
+    func kolodaDidRunOutOfCards(koloda: KolodaView) {
+        //Example: reloading
+        kolodaView.resetCurrentCardNumber()
+    }
+    
+    func kolodaDidSelectCardAtIndex(koloda: KolodaView, index: UInt) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
+    }
+    
+    func kolodaShouldApplyAppearAnimation(koloda: KolodaView) -> Bool {
+        return true
+    }
+    
+    func kolodaShouldMoveBackgroundCard(koloda: KolodaView) -> Bool {
+        return false
+    }
+    
+    func kolodaShouldTransparentizeNextCard(koloda: KolodaView) -> Bool {
+        return false
+    }
+    
+    func kolodaBackgroundCardAnimation(koloda: KolodaView) -> POPPropertyAnimation? {
+        let animation = POPSpringAnimation(propertyNamed: kPOPViewFrame)
+        animation.springBounciness = frameAnimationSpringBounciness
+        animation.springSpeed = frameAnimationSpringSpeed
+        return animation
     }
 
 
