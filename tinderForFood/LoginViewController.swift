92571ac9673f02dfb1ab1fcd07c3de010b0e4b9a
Edward Yun <edward.d.yun@vanderbilt.edu>
2015-12-03 14:37:37 -0600

    parsing json, YAY

--- tinderForFood/LoginViewController.swift
+++ tinderForFood/LoginViewController.swift
@@ -17,9 +17,13 @@ class LoginViewController: UIViewController {
     private var token = "ireJLoBsAiflg_HSScggpFpKTZ1giE8S"
     private var tokenSecret = "0hmsBuZ2hGtFTm80Gt-iveENveI"
     
+    private var accessToken = ""
+    
     @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmLabel: UILabel!
@@ -43,16 +47,16 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-//    func checkUsername() -> Bool {
-//        var flag = false
-//        makeCall("checkUsername", params: ["username": "edward123"]) { responseObject, error in
-//            print("response is \(responseObject); error is \(error)")
-//            if responseObject == 200 {
-//                flag = true
-//            }
-//        }
-//        return flag
-//    }
+    //    func checkUsername() -> Bool {
+    //        var flag = false
+    //        makeCall("checkUsername", params: ["username": "edward123"]) { responseObject, error in
+    //            print("response is \(responseObject); error is \(error)")
+    //            if responseObject == 200 {
+    //                flag = true
+    //            }
+    //        }
+    //        return flag
+    //    }
     
     /* CHECKUSERNAME FUNCTION */
     //    func checkUsername(username: String, completionHandler: (Int) -> ()) {
@@ -82,7 +86,7 @@ class LoginViewController: UIViewController {
     //    func makeCall(apiCall: String, params: [String:String], completionHandler: (NSString, NSError) -> ()) {
     func makeCall(apiCall: String, params: [String: String], completionHandler: (Int, JSON, NSError?) -> ()) {
         Alamofire.request(.POST, "https://tinder-for-food.herokuapp.com/api/\(apiCall)", parameters: params, encoding: .JSON)
-            .responseJSON { response in
+            .responseString { response in
                 print(response.request)  // original URL request
                 print(response.response) // URL response
                 print(response.data)     // server data
@@ -90,25 +94,46 @@ class LoginViewController: UIViewController {
                 
                 let json = JSON(response.data!)
                 
+                //                if let accessToken = json["access_token"] {
+                //                    print("printing \(accessToken)")
+                //                } else {
+                //                    print(json["access_token"].error)
+                //                }
                 
-                if let accessToken = json["access_token"].string {
-                    print("printing \(accessToken)")
-                } else {
-                    print(json["access_token"].error)
-                }
-
                 
                 completionHandler(response.response!.statusCode, json, response.result.error)
                 
         }
     }
     
+    func makeJsonCall(apiCall: String, params: [String: String], completionHandler: (Int, JSON, NSError?) -> ()) {
+        Alamofire.request(.POST, "https://tinder-for-food.herokuapp.com/api/\(apiCall)", parameters: params, encoding: .JSON).responseJSON { response in
+            
+            guard response.result.error == nil else {
+                // got an error in getting the data, need to handle it
+                print("error calling POST on /api/\(apiCall)")
+                print(response.result.error!)
+                return
+            }
+            
+            if let value: AnyObject = response.result.value {
+                // handle the results as JSON, without a bunch of nested if loops
+                let responseJson = JSON(value)
+
+                completionHandler(response.response!.statusCode, responseJson, response.result.error)
+            }
+            
+        }
+    }
+    
+    
+    
     
     //MARK: Storyboard
     
     @IBAction func loginButtonClicked(sender: AnyObject) {
         if loginBool {
-            checkThis()
+//            checkThis()
         } else {
             signup()
         }
@@ -123,15 +148,53 @@ class LoginViewController: UIViewController {
     }
     
     func signup() {
-        let parameters = ["username": "edward22", "email": "edward22@gmail.com", "password": "password"]
-        makeCall("register", params: parameters) { responseCode, responseJson, error in
-            print(responseCode)
-            print(responseJson)
+        
+        let email = emailTextField.text
+        let username = usernameTextField.text
+        let password = passwordTextField.text
+        let confirm = confirmTextField.text
+        
+        if password != confirm  {
+            let alertController = UIAlertController(title: "Oops", message:
+                "Make sure your passwords match!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
         }
+        else if password?.characters.count < 5 {
+            let alertController = UIAlertController(title: "Oops", message:
+                "Password must be at least 5 characters", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+        }
+        else if username?.characters.count < 5 {
+            let alertController = UIAlertController(title: "Oops", message:
+                "Username must be at least 5 characters", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        else if email?.characters.count < 5 {
+            let alertController = UIAlertController(title: "Oops", message:
+                "Email must be at least 5 characters", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        else {
+            checkUsername(username!)
+            let parameters = ["username": username!, "email": email!, "password": password!]
+            makeJsonCall("register", params: parameters) { responseCode, responseJson, error in
+                print(responseCode)
+                self.accessToken = responseJson["access_token"].stringValue
+            }
+        }
+        
     }
     
-    func checkThis() {
-        let parameters = ["username": "avery"]
+    func checkUsername(username: String) {
+        let parameters = ["username": username]
         makeCall("checkUsername", params: parameters) { responseCode, responseJson, error in
             print(responseCode)
         }
@@ -139,29 +202,29 @@ class LoginViewController: UIViewController {
     
     @IBAction func segmentChanged(sender: AnyObject) {
         switch loginSegmentedControl.selectedSegmentIndex {
-            case 0:
-                loginSelected()
-            case 1:
-                signupSelected()
-            default:
-                break
+        case 0:
+            loginSelected()
+        case 1:
+            signupSelected()
+        default:
+            break
         }
     }
     
     func loginSelected() {
         print("login")
-        confirmLabel.hidden = false
-        confirmTextField.hidden = false
+        confirmLabel.hidden = true
+        confirmTextField.hidden = true
         loginButton.setTitle("Login", forState: .Normal)
         loginBool = true
     }
     func signupSelected() {
         print("signup")
-        confirmLabel.hidden = true
-        confirmTextField.hidden = true
+        confirmLabel.hidden = false
+        confirmTextField.hidden = false
         loginButton.setTitle("Signup", forState: .Normal)
         loginBool = false
-
+        
     }
     
 }
